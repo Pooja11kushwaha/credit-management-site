53a805bae758b8a489cc715172d7f452bcc3a9a2
Pooja11kushwaha <53234832+Pooja11kushwaha@users.noreply.github.com>
2020-07-14 17:09:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<section class="text-gray-700 body-font">
+    <div class="container px-5 py-24 mx-auto flex flex-wrap">
+      <h2 class="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5">CREDIT MANGEMENT SYSTEM</h2>
+      <div class="md:w-3/5 md:pl-6">
+        <p class="leading-relaxed text-base"> This website can be used to transfer credits from one user to another. Also,
+            records all transfers happened.
+            </p>
+        <div class="flex md:mt-4 mt-6">
+          <button class="inline-flex text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded" onclick="location.href='user.php'">Let's get started</button>
+          
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</body>
+</html>
